0a1484b663a5facbbbed9dc56247aad0a56622a4
smetdenis <admin@jbzoo.com>
2016-08-04 21:39:33 +0700

    fix php version

--- www/index.php
+++ www/index.php
@@ -1,19 +1,29 @@
 <?php
 
-use SebastianBergmann\CodeCoverage\CodeCoverage;
-
 require_once __DIR__ . '/../vendor/autoload.php';
 
 
-$codeCoverage = new CodeCoverage();
+$hasXDebug = extension_loaded('xdebug');
 
-/**
- * It seems the fail is hear
- * Only PHP7 + xdebug in PHP built-in server
- * For PHP_SAPI == 'cli-server'
- */
-$codeCoverage->start('id-42', true);
+if ($hasXDebug) {
 
-require_once __DIR__ . '/included_file.php';
+    if (class_exists('SebastianBergmann\CodeCoverage\CodeCoverage')) {
+        $codeCoverage = new SebastianBergmann\CodeCoverage\CodeCoverage();
+    } else {
+        $codeCoverage = new PHP_CodeCoverage();
+    }
 
-$codeCoverage->stop();
+    /**
+     * It seems the fail is hear
+     * Only PHP7 + xdebug in PHP built-in server
+     * For PHP_SAPI == 'cli-server'
+     */
+    $codeCoverage->start('id-42', true); // <===
+
+
+    require_once __DIR__ . '/included_file.php';
+    $codeCoverage->stop();
+
+} else { // clean require
+    require_once __DIR__ . '/included_file.php';
+}
